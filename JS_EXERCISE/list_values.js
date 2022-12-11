c8b3208f4828dca6ee32a27de87d2b5bbf21918e
Mikko Saari <mikko.l.saari@tuni.fi>
2022-12-11 10:28:34 +0200

    growing_too_much

--- JS_EXERCISE/list_values.js
+++ JS_EXERCISE/list_values.js
@@ -1,7 +1,9 @@
 var assert = require('assert')
 
 const arr=['a','b','c','d']
+const brr=['a','b','c','d']
 assert.equal(arr.length,4);
+
 const object={
     first:'MyName',
     last: 'YourName',
@@ -15,7 +17,12 @@ function print(a){
     console.log("Moi" + a)
 }
 function check(){
-//    assert.equal(print(1),Moi0)
+    for(vbl of brr){
+	for(val of arr){
+	    assert.equal(console.log(val),console.log(vbl))
+	}
+    }
+    //    assert.equal(print(1),Moi0)
 }
 print(1)
 check();
